6fbe50f7458c38e874cd52e8010643c1bceba3ec
Manish Thakur <tmanish900@gmail.com>
2026-01-19 12:47:45 +0530

    error solved

--- src/app/scorebook/addteam/page.jsx
+++ src/app/scorebook/addteam/page.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import styles from './addTeam.module.scss'
 import AddTeamComp from '@/components/addTeam/addTeam'
+import { Suspense } from "react";
 
 const AddTeam = () => {
   
   return (
     <>
-      <AddTeamComp />  
+      <Suspense fallback={<div>Loading...</div>}>
+        <AddTeamComp />        
+      </Suspense>
     </>
   )
 }
